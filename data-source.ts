dc64137e9c56321f000f1d56cda8af06ce17bfc6
Justin Obi <justin@Justins-MacBook-Pro.local>
2023-09-02 23:29:09 +0100

    feat: quiz

--- data-source.ts
+++ data-source.ts
@@ -1,34 +1,69 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+import { ConfigService } from '@nestjs/config'
 
-export const dataSourceOptions = async (configService: ConfigService): Promise<TypeOrmModuleOptions> => {
+// dev
+// export const dataSourceOptions = async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
+//   type: 'mysql', // Database type
+//   host: configService.get<string>('DB_HOST'),
+//   port: configService.get<number>('DB_PORT'),
+//   username: configService.get<string>('DB_USERNAME'),
+//   password: configService.get<string>('DB_PASSWORD'),
+//   database: configService.get<string>('DB_DATABASE'),
+//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//   synchronize: true,
+//   logging: true
+// })
 
-  const isProduction = configService.get<string>('NODE_ENV') === 'production'
-
-  const baseOptions: TypeOrmModuleOptions = {
-    type: 'mysql', // Database type
-    username: configService.get<string>('DB_USERNAME'),
-    password: configService.get<string>('DB_PASSWORD'),
-    database: configService.get<string>('DB_DATABASE'),
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
-    logging: true,
-  };
-
-  if (isProduction) {
-    return {
-      ...baseOptions,
-      host: 'localhost',
-      port: configService.get<number>('DB_PORT'),
-      extra: {
-        socketPath: configService.get<string>('CONNECTION_NAME'),
-      },
-    };
-  } else {
-    return {
-      ...baseOptions,
-      host: configService.get<string>('DB_HOST'),
-      port: configService.get<number>('DB_PORT'),
-    };
+// Prod
+export const dataSourceOptions = async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
+  type: 'mysql', // Database type
+  host: 'localhost',
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USERNAME'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_DATABASE'),
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
+  logging: true,
+  extra: {
+    socketPath: configService.get<string>('CONNECTION_NAME')
   }
-};
+})
+
+
+
+
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+// import { ConfigService } from '@nestjs/config';
+
+// export const dataSourceOptions = async (configService: ConfigService): Promise<TypeOrmModuleOptions> => {
+
+//   const isProduction = configService.get<string>('NODE_ENV') === 'production'
+
+//   const baseOptions: TypeOrmModuleOptions = {
+//     type: 'mysql', // Database type
+//     username: configService.get<string>('DB_USERNAME'),
+//     password: configService.get<string>('DB_PASSWORD'),
+//     database: configService.get<string>('DB_DATABASE'),
+//     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//     synchronize: true,
+//     logging: true,
+//   };
+
+//   if (isProduction) {
+//     return {
+//       ...baseOptions,
+//       host: 'localhost',
+//       port: configService.get<number>('DB_PORT'),
+//       extra: {
+//         socketPath: configService.get<string>('CONNECTION_NAME'),
+//       },
+//     };
+//   } else {
+//     return {
+//       ...baseOptions,
+//       host: configService.get<string>('DB_HOST'),
+//       port: configService.get<number>('DB_PORT'),
+//     };
+//   }
+// };
