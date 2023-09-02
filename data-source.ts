6515f3cfbbdfa2d96c44cf347f21bb27bfb140c5
Justin Obi <justin@Justins-MacBook-Pro.local>
2023-09-02 13:55:54 +0100

    Cloud build script

--- data-source.ts
+++ data-source.ts
@@ -4,12 +4,18 @@ import { ConfigService } from '@nestjs/config'
 
 export const dataSourceOptions = async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
   type: 'mysql', // Database type
-  host: configService.get<string>('DB_HOST'),
+  host: 'localhost',
+  // host: configService.get<string>('DB_HOST'),
   port: configService.get<number>('DB_PORT'),
   username: configService.get<string>('DB_USERNAME'),
   password: configService.get<string>('DB_PASSWORD'),
   database: configService.get<string>('DB_DATABASE'),
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true,
-  logging: true
+  logging: true,
+  extra: {
+    socketPath: configService.get<string>('CONNECTION_NAME')
+  }
 })
+
+// DATABASE_URL="postgresql://zuser:pass@localhost/db?host=/cloudsql/projectis:us-central1:db"
---
+++ gcloud-build-and-deploy.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+export PROJECT_ID=treeviah
+export REGION=us-east1
+export CONNECTION_NAME=treeviah:us-central1:myinstance
+
+gcloud builds submit \
+  --tag gcr.io/$PROJECT_ID/poll \
+  --project $PROJECT_ID
+
+gcloud run deploy poll \
+  --image gcr.io/$PROJECT_ID/poll \
+  --platform managed \
+  --region $REGION \
+  --allow-unauthenticated \
+  --add-cloudsql-instances $CONNECTION_NAME \
+  --project $PROJECT_ID
+
+  # gcloud run deploy treeviah \
+  # --image gcr.io/treeviah/treeviah \
+  # --platform managed \
+  # --region us-east1 \
+  # --allow-unauthenticated \
+  # --add-cloudsql-instances treeviah:us-central1:myinstance \
+  # --project treeviah
