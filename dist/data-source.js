0ddf49fc1dccddd4dd3d83e9e0956346a6ea8a96
Justin Obi <justin@Justins-MacBook-Pro.local>
2023-10-16 06:01:17 +0100

    New build

--- dist/data-source.js
+++ dist/data-source.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.dataSourceOptions = void 0;
 const dataSourceOptions = async (configService) => ({
     type: 'mysql',
-    host: configService.get('NODE_ENV') === 'staging' ? 'localhost' : configService.get('DB_HOST'),
+    host: configService.get('NODE_ENV') === 'staging' ? 'mysql://treeviah:treeviah@db4free.net:3306/treeviah' : configService.get('DB_HOST'),
     port: configService.get('DB_PORT'),
     username: configService.get('DB_USERNAME'),
     password: configService.get('DB_PASSWORD'),
@@ -11,9 +11,6 @@ const dataSourceOptions = async (configService) => ({
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
     synchronize: true,
     logging: true,
-    extra: {
-        socketPath: configService.get('NODE_ENV') === 'staging' ? configService.get('CONNECTION_NAME') : ''
-    }
 });
 exports.dataSourceOptions = dataSourceOptions;
 //# sourceMappingURL=data-source.js.map
