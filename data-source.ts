aacd389c82a760a047a79b8b3a80207b65788c7c
Justin Obi <justin@Justins-MacBook-Pro.local>
2023-12-20 22:20:35 +0100

    Back to render platform

--- data-source.ts
+++ data-source.ts
@@ -4,8 +4,8 @@ import { ConfigService } from '@nestjs/config'
 
 export const dataSourceOptions = async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
   type: 'mysql',
-  host: configService.get<string>('NODE_ENV') === 'staging' ? 'localhost' : configService.get<string>('DB_HOST'),
-  // url: configService.get<string>('NODE_ENV') === 'staging' ? 'mysql://treeviah:treeviah@db4free.net:3306/treeviah' : configService.get<string>('DB_HOST'),
+  // host: configService.get<string>('NODE_ENV') === 'staging' ? 'localhost' : configService.get<string>('DB_HOST'),
+  url: configService.get<string>('NODE_ENV') === 'development' ? 'mysql://treeviah:treeviah@db4free.net:3306/treeviah' : configService.get<string>('DB_HOST'),
   port: configService.get<number>('DB_PORT'),
   username: configService.get<string>('DB_USERNAME'),
   password: configService.get<string>('DB_PASSWORD'),
